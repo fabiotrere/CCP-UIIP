94f2d9c7e25e060cd827ebbc0abc69e0f7012a4a
Fabio Trerè <fabio.trere01@gmail.com>
2020-11-09 23:09:16 +0100

    Menu iniziale 1.0 Connessione a my sql

--- Gestione_Contatori.py
+++ Gestione_Contatori.py
@@ -1,3 +1,17 @@
+import os
+os.chdir('C:/Users/fabio/Desktop/Code contest project/')
+import pymysql
+
+db = pymysql.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     db="ccp")            # name of the data base
+cur = db.cursor()
+
+cur.execute('INSERT INTO banche(id_banca, nome_banca, indirizzo_fatturazione) VALUES (001, "Banca di Milano", "Via Verdi 5"),(002, "Banca di Milano", "Via Verdi 5"),(003, "Banca di Milano", "Via Verdi 5")')
+cur.execute("SELECT * FROM banche")
+db.commit()
+for row in cur.fetchall():
+    print (row)
 def rimuovi_cliente(nome_da_eliminare):
     pass
 def stampa_clienti():
@@ -17,8 +31,9 @@ def stampa_comandi():
     print("*******************")
 #___MAIN___
 print("Benvenuto nel software di gestione contatori")
-while True:
+while False:
     comando = int(input("Scegli il comando da eseguire: "))
+    stampa_comandi()
     if comando == 1:
         nome_da_eliminare = input("inserisci il nome dell'utente da eliminare: ")
         rimuovi_cliente(nome_da_eliminare)
@@ -34,6 +49,8 @@ while True:
         pass
     elif comando == 7:
         stampa_comandi()
+    elif comando == 8:
+        pass
     elif comando == 0:
         print('Grazie per aver usato la nostra applicazione')
         break
