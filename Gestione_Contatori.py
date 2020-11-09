89bcc286b8b1451ab89cca5c7ff5897e6aea6f77
Fabio Trerè <fabio.trere01@gmail.com>
2020-11-09 21:38:46 +0100

    Menu iniziale 1.0

--- Gestione_Contatori.py
+++ Gestione_Contatori.py
@@ -1,42 +1,41 @@
+def rimuovi_cliente(nome_da_eliminare):
+    pass
+def stampa_clienti():
+    pass
+def nuovo_cliente():
+    pass
+def stampa_comandi():
+    print("****************")
+    print("1 Cancella utente")
+    print("2 Stampa elenco clienti")
+    print("3 ...")
+    print("4 ...")
+    print("5 Inserisci nuovo cliente")
+    print("6 Modifica contatto")
+    print("7 Stampa Comandi")
+    print("0 Quit")
+    print("*******************")
+#___MAIN___
+print("Benvenuto nel software di gestione contatori")
 while True:
-    stampa_comandi()
     comando = int(input("Scegli il comando da eseguire: "))
     if comando == 1:
-        nome_eliminare = input("inserisci il nome dell'utente da eliminare: ")
-        cancella_utente(nome_eliminare)
+        nome_da_eliminare = input("inserisci il nome dell'utente da eliminare: ")
+        rimuovi_cliente(nome_da_eliminare)
     elif comando == 2:
-        stampa_rubrica()
+        stampa_clienti()
     elif comando == 3:
-        print("Marco Verdi", rubrica['Marco Verdi'])
-        print("Mario Rossi", rubrica['Mario Rossi'])
-        print("Luca Bianchi", rubrica['Luca Bianchi'])
-
-
+        pass
     elif comando == 4:
-        print("Luca Bianchi", rubrica['Luca Bianchi'])
-        print("Mario Rossi", rubrica['Mario Rossi'])
-        print("Marco Verdi", rubrica['Marco Verdi'])
-
-
+        pass
     elif comando == 5:
-        nuovo_contatto()
+        nuovo_cliente()
     elif comando == 6:
-        nome = input("inserisci nome")
-        lista_dati = ottieni_lista(nome)
-        print("scegliere uno dei seguenti dati da modificare:")
-        print("via")
-        print("CAP")
-        print("citta")
-        print("telefono")
-        print("e_mail")
-        print("campo_note")
-        dato_da_modificare = input("> ")
-
-        nuovo_dato = input("inserisci il nuovo dato")
-        rubrica[nome] = modifica_dati(lista_dati, dato_da_modificare, nuovo_dato)
+        pass
+    elif comando == 7:
+        stampa_comandi()
     elif comando == 0:
         print('Grazie per aver usato la nostra applicazione')
         break
-
     else:
         print("comando non riconosciuto")
