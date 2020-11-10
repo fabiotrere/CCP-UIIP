6d1a94971e4baa8ecb4108f7b3ab4d0a0154b9bf
CARMELA <griecocarmela95@gmail.com>
2020-11-10 12:39:46 +0100

    update2

--- Gestione_Contatori.py
+++ Gestione_Contatori.py
@@ -1,3 +1,17 @@
+import pymysql
+db = pymysql.connect(host="localhost",                    # your host, usually localhost
+                     user="root",                         # your username
+                     db="gestione_contatori")             # name of the data base
+cur = db.cursor()
+
+cur.execute('INSERT INTO banche(id_banca, nome_banca, indirizzo_fatturazione) VALUES (001, "Banca di Milano", "Via Verdi 5"),(002, "Banca di Pescara", "Via Verdi 5"),(003, "Banca di Firenze", "Via Rossi 5"),(004, "Banca di Pisa", "Via del Corso 5"),(005, "Banca di Torino", "Via Roma 15"),(006, "Banca di Bari", "Via Sparano 5"), (007, "Banca di Venezia", "Via Capri 7"),(008, "Unicredit Banca", "Via Battisti 21"), (009, "Banca Intesa", "Via Liguria 43"),(010, "Banca di Bologna", "Via Leonida 5")')
+cur.execute("SELECT * FROM banche")
+db.commit()
+
+cur.execute('INSERT INTO utenti(id_utente, CF, nome, cognome, indirizzo, città, CAP, tipologia_cliente, moroso) VALUES (001, "RSSMRA68S14D643C", "Mario", "Rossi", "Via Campania 9", "Taranto", "74121", "azienda", "NO"),(002, "VRDGLI68S14D643W", "Giulia", "Verdi", "Via Mirabella 33", "Foggia", "71021", "privato", "NO"),(003, "BLLLSN82A01H501P", "Alessandro", "Bellino", "Via Roma 6", "Roma", "00145", "azienda", "NO"),(004, "DPLFNC60R54D643P", "Francesco", "Depaolis", "Via Firenze 10", "Bari", "72987", "privato", "NO"),(005, "BRNCRL74A01F205Z", "Carla", "Bernabei", "Via Castelfidardo 3", "Milano", "20122", "azienda", "NO"),(006, "GRCNGL73R54D643R", "Angela", "Grieco", "Via Cesare Battisti 56", "Roma", "00145", "privato", "SI"), (007, "CNTGNI85A01F205K", "Gina", "Conte", "Via Marconi 50", "Roma", "00147", "azienda", "NO"),(008, "RSSMHL73R54D643T", "Michele", "Rossi", "Via Mazzini 4", "Torino", "10121", "privato", "SI"), (009, "MRZFLV85A01L219V", "Flavio", "Marzocca", "Via Amendola 90", "Firenze", "00354", "azienda", "NO"),(010, "CRDCLD94E45L049W", "Claudia", "Cardella", "Via Veneto 89", "Roma", "00148", "privato", "NO")')
+cur.execute("SELECT * FROM utenti")
+db.commit()
+
 def rimuovi_cliente(nome_da_eliminare):
     pass
 def stampa_clienti():
