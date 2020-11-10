c81151b5f16509e8dd258d56ce11f9b2c785c3b8
Fabio Trerè <fabio.trere01@gmail.com>
2020-11-10 15:43:56 +0100

    4 funzioni OK

--- Gestione_Contatori.py
+++ Gestione_Contatori.py
@@ -5,33 +5,29 @@ db = pymysql.connect(host="localhost",    # your host, usually localhost
 cur = db.cursor()
 def rimuovi_riga():
     try:
-        #global lista_colonne
-        scegli_tabella()
-        lista_colonne = []
-        print ("*1")
-        cur.execute('SHOW COLUMNS FROM ' + tabella_scelta)
-        for row in cur:
-            name = row[0]
-            lista_colonne.append(name)
-        print("*2")
+        lista_colonne = seleziona_lista_colonne()
         id_da_eliminare = input('inserisci l\'id dell\'oggetto da eliminare: ')
         cur.execute("DELETE FROM " + tabella_scelta + " WHERE " + lista_colonne[0] + " = "+ id_da_eliminare)
         db.commit()
     except:
         db.rollback
         print ("ERRORE")
-def modifica_riga():
+def seleziona_lista_colonne():
     scegli_tabella()
     lista_colonne = []
-    lista_tipo = []
     cur.execute('SHOW COLUMNS FROM ' + tabella_scelta)
     for row in cur:
         name = row[0]
-        tipo_dati = row[1]
         lista_colonne.append(name)
-        lista_tipo.append(tipo_dati)
-    lista_valori = []
-
+    return lista_colonne
+def modifica_riga():
+    lista_colonne = seleziona_lista_colonne()
+    print(lista_colonne)
+    selezione_campo = input("inserisci il campo da modificare: ")
+    selezione_nuovo_campo = input("inserisci il nuovo valore del campo: ")
+    selezione_id = input("inserisci l'id della riga da modificare: ")
+    cur.execute('UPDATE ' + tabella_scelta + ' SET ' + selezione_campo + ' = ' + '"' + selezione_nuovo_campo + '"' + ' WHERE ' + tabella_scelta + '.'+ lista_colonne[0] + ' = ' + selezione_id)
+    db.commit()
 def aggiungi_osservazione():
     try:
         scegli_tabella()
@@ -64,14 +60,23 @@ def scegli_tabella():
         tabella_scelta = input('> Inserisci il nome di una tabella: ')
     except:
         print("Tabella non riconosciuta")
+def seleziona_id():
+    global id_scelto
+    id_scelto =  input('Inserire id dell\'elemento > ')
+def leggi_tabella():
+    seleziona_id()
+    lista_colonne = seleziona_lista_colonne()
+    cur.execute('SELECT * FROM ' + tabella_scelta + ' WHERE ' + lista_colonne[0] + '=' + id_scelto)
+    for row in cur:
+        print(row)
 def stampa_clienti():
     pass
 def stampa_comandi():
     print("****************")
     print("1 Cancella utente")
     print("2 ...")
-    print("3 ...")
-    print("4 ...")
+    print("3 Modifica Riga")
+    print("4 Leggi una riga di una tabella")
     print("5 Inserisci una nuova riga in una tabella a tua scelta")
     print("6 ...")
     print("7 ...")
@@ -87,9 +92,9 @@ while True:
     elif comando == 2:
         stampa_clienti()
     elif comando == 3:
-        pass
+        modifica_riga()
     elif comando == 4:
-        pass
+        leggi_tabella()
     elif comando == 5:
         aggiungi_osservazione()
     elif comando == 6:
